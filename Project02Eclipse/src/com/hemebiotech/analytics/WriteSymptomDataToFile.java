90322c02866ae0880a1ff20a34a68bcf5d7e8b46
Jérémy Radenne <jeremy@MacBook-Air-de-Jeremy.local>
2024-11-15 17:08:39 +0100

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,22 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("Project02Eclipse/result.out"))) {
+            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                writer.write(entry.getKey() + ":" + entry.getValue());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing file: " + e.getMessage());
+        }
+    }
+}
