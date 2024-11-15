72d67ca33f99cbda04ac4402592f48c51f7f5336
Jérémy Radenne <jeremy@MacBook-Air-de-Jeremy.local>
2024-11-15 18:24:37 +0100

    main implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,13 +5,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter implements ISymptomReader, ISymptomWriter {
-	public static void main(String[] args) {
-		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
-		List<String> symptoms = analyticsCounter.getSymptoms();
-		Map<String, Integer> symptomsMap = analyticsCounter.countSymptoms(symptoms);
-		analyticsCounter.writeSymptoms(symptomsMap);
-	}
-	
+
 	@Override
 	public List<String> getSymptoms() {
 		return new ReadSymptomDataFromFile().getSymptoms();
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,13 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        List<String> symptoms = analyticsCounter.getSymptoms();
+        Map<String, Integer> symptomsMap = analyticsCounter.countSymptoms(symptoms);
+        analyticsCounter.writeSymptoms(symptomsMap);
+	}
+}
