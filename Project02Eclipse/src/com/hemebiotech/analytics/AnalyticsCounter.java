d40ff2a6bb7767eacfaec7f84722cb77d832b1c6
Jérémy Radenne <jeremy@MacBook-Air-de-Jeremy.local>
2024-11-15 18:15:39 +0100

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,32 +1,31 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class AnalyticsCounter {
+public class AnalyticsCounter implements ISymptomReader, ISymptomWriter {
+	public static void main(String[] args) {
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		List<String> symptoms = analyticsCounter.getSymptoms();
+		Map<String, Integer> symptomsMap = analyticsCounter.countSymptoms(symptoms);
+		analyticsCounter.writeSymptoms(symptomsMap);
+	}
+	
+	@Override
+	public List<String> getSymptoms() {
+		return new ReadSymptomDataFromFile().getSymptoms();
+	}
 
-    public static void main(String[] args) {
-        Map<String, Integer> symptomsMap = new TreeMap<>();
-
-        try {
-            List<String> symptomsList = Files.readAllLines(Paths.get("Project02Eclipse/symptoms.txt"));
-
-            for (String symptom : symptomsList) symptomsMap.put(symptom, symptomsMap.getOrDefault(symptom, 0) + 1);
-            
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Project02Eclipse/result.out"))) {
-                for (Map.Entry<String, Integer> entry : symptomsMap.entrySet()) {
-                    writer.write(entry.getKey() + ":" + entry.getValue());
-                    writer.newLine();
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred: " + e.getMessage());
-        }
-    }
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> symptomsMap = new TreeMap<>();
+		for (String symptom : symptoms)
+			symptomsMap.put(symptom, symptomsMap.getOrDefault(symptom, 0) + 1);
+		return symptomsMap;
+	}
+	
+	@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		new WriteSymptomDataToFile().writeSymptoms(symptoms);
+	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,47 +1,24 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Simple brute force implementation
- *
- */
 public class ReadSymptomDataFromFile implements ISymptomReader {
-
-	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
 	
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+	public List<String> getSymptoms() {
+		List<String> symptoms = new ArrayList<>();
+
+		try {
+			symptoms = Files.readAllLines(Paths.get("Project02Eclipse/symptoms.txt"));
+		} catch (IOException e) {
+			System.out.println("An error occurred: " + e.getMessage());
 		}
 		
-		return result;
+		return symptoms;
 	}
 
 }
